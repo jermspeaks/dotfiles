0eb3621a8823164fddf3139830663e5fe9f61bf1
jermspeaks <jermspeaks@gmail.com>
2017-08-14 09:58:59 -0700

    open git window to install latest

--- vim/install.sh
+++ vim/install.sh
@@ -6,3 +6,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Installing preset plugins"
 vim +PluginInstall +qall
+
+echo "update git to the latest version:"
+echo "https://git-scm.com/download/mac"
+
+open "https://git-scm.com/download/mac"
